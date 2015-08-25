1c29a79409bf06fe1bfdee0aa6b3dbfc7e2ae843
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2015-08-25 11:05:56 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+'''
+Installs ecs
+'''
+from setuptools import setup
+
+from ecs import __version__
+
+setup(
+    name='ecs',
+    version=__version__,
+    license='Apache 2.0',
+)
