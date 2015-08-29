12ca169a03651f4b85effb00928602fcc10cbea3
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2015-08-29 06:53:24 -0500

    Updated options and command-line interface.
    * Added option for quiet.
    * Command line interface can now be imported for profiling

--- examples/combat_sim/run.py
+++ examples/combat_sim/run.py
@@ -8,11 +8,15 @@ Usage: run [options]
 Options:
     -c --cowboys NUMBER    Number of cowboys [default: 10]
     -a --aliens NUMBER     Number of aliens [default: 10]
+    -q --quiet             Reduces verbosity
 '''
 from entities import setup_entities
 from systems import CombatSystem
 
-def simulate(number_of_cowboys, number_of_aliens):
+from memory_profiler import profile
+
+# @profile
+def simulate(number_of_cowboys, number_of_aliens, quiet=False):
     '''Simulates combat between aliens and cowboys'''
     combat_system = CombatSystem()
 
@@ -22,14 +26,17 @@ def simulate(number_of_cowboys, number_of_aliens):
     print('='*40)
     print(' Combat Sim: Cowboys vs Aliens')
     print('-'*40)
-    print("Round: # -- C | A")
-    print('-----------------')
+    if not quiet:
+        print("Round: # -- C | A")
+        print('-----------------')
     while True:
         cowboys_alive = sum(e.health.alive for e in entities['cowboys'])
         aliens_alive = sum(e.health.alive for e in entities['aliens'])
         if cowboys_alive == 0 or aliens_alive == 0:
             break
-        print("Round: {} -- {} | {}".format(combat_round, cowboys_alive, aliens_alive))
+        if not quiet:
+            results = "Round: {} -- {} | {}"
+            print(results.format(combat_round, cowboys_alive, aliens_alive))
         combat_round += 1
         while combat_system.update() is not None:
             continue
@@ -50,14 +57,23 @@ def simulate(number_of_cowboys, number_of_aliens):
         print(' Aliens win')
     else:
         print(' Everyone died!')
+    print('\n')
 
 
-if __name__ == '__main__':
+def handle_cli():
     from docopt import docopt
 
     args = docopt(__doc__)
 
-    number_of_cowboys = int(args.get('--cowboys'))
-    number_of_aliens = int(args.get('--aliens'))
+    kwds = {
+        'number_of_cowboys': int(args.get('--cowboys')),
+        'number_of_aliens': int(args.get('--aliens')),
+        'quiet': args.get('--quiet', None)
+    }
 
-    simulate(number_of_cowboys, number_of_aliens)
+    return kwds
+
+if __name__ == '__main__':
+    kwds = handle_cli()
+    simulate(**kwds)
+
