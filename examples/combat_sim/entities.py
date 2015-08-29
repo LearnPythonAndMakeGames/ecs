3471b9a8e5da8b8d6fd42427bc5ecf31284b9f28
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2015-08-29 07:34:39 -0500

    More tweaks to numbers
    * Alien health increased slightly
    * Cowboy critical strike percentage nearly doubled

--- examples/combat_sim/entities.py
+++ examples/combat_sim/entities.py
@@ -15,14 +15,14 @@ def setup_entities(number_of_cowboys=100, number_of_aliens=100):
     for cowboy in range(number_of_cowboys):
         cowboy = Entity('cowboy-{:02}'.format(cowboy))
         cowboy.health = Health(cowboy, current=51, max=53)
-        cowboy.damage = Damage(cowboy, normal=12, critical=17, critical_percent=10)
+        cowboy.damage = Damage(cowboy, normal=12, critical=17, critical_percent=19)
         cowboy.humanoid = 'cowboy'
         cowboys.append(cowboy)
 
     aliens = []
     for alien in range(number_of_aliens):
         alien = Entity('alien-{:02}'.format(alien))
-        alien.health = Health(alien, current=100, max=100)
+        alien.health = Health(alien, current=102, max=102)
         alien.damage = Damage(alien, normal=6, critical=8, critical_percent=20)
         alien.humanoid = 'alien'
         aliens.append(alien)
